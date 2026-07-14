ed4f5b0b625e41211bde24f6949a2b90cd8447ae
John Pollock <pollockjj@gmail.com>
2026-07-14 03:18:03 -0500

    Revert "Test warp-N128 packed INT8 grouped GEMM"
    This reverts commit a4b988436aad9a45d106a400861a721428a68810.

--- comfy_kitchen/backends/cuda/ops/cutlass_gemm_int8.cu
+++ comfy_kitchen/backends/cuda/ops/cutlass_gemm_int8.cu
@@ -462,7 +462,7 @@ using PackedInt8KernelT = typename cutlass::gemm::kernel::DefaultGemmGrouped<
     cutlass::arch::OpClassTensorOp,
     cutlass::arch::Sm80,
     cutlass::gemm::GemmShape<64, 128, 64>,
-    cutlass::gemm::GemmShape<32, 128, 64>,
+    cutlass::gemm::GemmShape<32, 64, 64>,
     cutlass::gemm::GemmShape<16, 8, 32>,
     PackedInt8Epilogue,
     cutlass::gemm::threadblock::GemmBatchedIdentityThreadblockSwizzle,
